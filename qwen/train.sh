e327467678ec2c1a84088fd04decb3d05ee99059
zhangyue <okeeper.com@gmail.com>
2025-05-13 20:25:44 +0800

    ddd

--- qwen/train.sh
+++ qwen/train.sh
@@ -1,15 +1,19 @@
 MODEL_PATH=/data/hf-models/Qwen3-8B
 
+# 设置环境变量
+export CUDA_VISIBLE_DEVICES=5,6,7
+
 # 全参数微调
 llamafactory-cli train \
-    --model_name_or_path ${MODEL_PATH} \
+    --model_name_or_path /data/hf-models/Qwen3-8B \
     --trust_remote_code \
     --stage sft \
     --do_train \
+    --deepspeed examples/deepspeed/my_ds.json \
     --finetuning_type full \
-    --dataset xd_final_sft,xd_pretain_mix \
+    --dataset xd_final_sft \
     --template qwen3 \
-    --cutoff_len 2048 \
+    --cutoff_len 512 \
     --max_samples 10000 \
     --overwrite_cache \
     --preprocessing_num_workers 16 \
@@ -17,15 +21,13 @@ llamafactory-cli train \
     --output_dir saves/qwen3_full_sft \
     --logging_steps 1 \
     --save_steps 500 \
-    --plot_loss \
     --overwrite_output_dir \
     --save_only_model false \
     --report_to none \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 2 \
     --learning_rate 1e-5 \
     --num_train_epochs 2.0 \
     --lr_scheduler_type cosine \
     --warmup_ratio 0.1 \
-    --bf16 \
-    --ddp_timeout 180000000
+    --bf16
